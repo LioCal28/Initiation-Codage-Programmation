5da948e35fb41b3a47fc6f49443f411834604e16
LioCal28 <lionel397@hotmail.be>
2025-05-07 15:52:26 +0200

    Add files via upload

---
+++ Exo Pendu/Program.cs
@@ -0,0 +1,18 @@
+﻿string[] listeMots = new string[10];
+string[] MOTS = { "Macron", "Le Pen", "Attal", "Bayrou", "Mélenchon", "Chirac", "Hollande", "Sarkozy", "Giscard", "Zemmour" };
+Random random = new Random();
+int nombre = random.Next(0,10);
+
+Console.WriteLine(MOTS[nombre]);
+
+Console.WriteLine("---------");
+Console.WriteLine(MOTS[0]);
+Console.WriteLine(MOTS[1]);
+Console.WriteLine(MOTS[2]);
+Console.WriteLine(MOTS[3]);
+Console.WriteLine(MOTS[4]);
+Console.WriteLine(MOTS[5]);
+Console.WriteLine(MOTS[6]);
+Console.WriteLine(MOTS[7]);
+Console.WriteLine(MOTS[8]);
+Console.WriteLine(MOTS[9]);
---
+++ Exo Tableau2/Program.cs
@@ -0,0 +1,18 @@
+﻿//Exercice moyenne des scores  (page 127) :
+//Écrire un algorithme demandant à l’utilisateur le nombre de joueurs (max 10 joueurs). Ensuite, l’algorithme doit demander à l’utilisateur le score de chaque joueur. Une fois ceci fini, il faut afficher la moyenne des scores. 
+Console.WriteLine("Combien de joueurs avez-vous dans votre partie ?");
+int nbrJoueurs = int.Parse(Console.ReadLine());
+while (nbrJoueurs > 10 || nbrJoueurs < 1)
+{
+    Console.WriteLine($"Vous avez choisi {nbrJoueurs} joueurs. Veuillez entrer un nombre de joueurs compris entre 1 et 10.");
+    nbrJoueurs = int.Parse(Console.ReadLine());
+}
+int[] tabJoueurs = new int[nbrJoueurs];
+Console.WriteLine($"Vous avez donc {nbrJoueurs} joueurs dans votre partie. Veuillez entrer les scores des joueurs afin d'obtenir la moyenne.");
+for (int i = 0; i < tabJoueurs.Length; i++)
+{
+    Console.WriteLine($"Entrez le score du joueur n°{i + 1}");
+    tabJoueurs[i] = int.Parse(Console.ReadLine());
+}
+int sum = tabJoueurs.Sum();
+Console.WriteLine($"La moyenne des {nbrJoueurs} joueurs est de {sum / nbrJoueurs}");
