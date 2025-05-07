0f2097349925f10127dff0de2dbf0358578fde62
LioCal28 <lionel397@hotmail.be>
2025-05-07 15:56:15 +0200

    Add files via upload

---
+++ Test ASCII/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        string asciiImage = File.ReadAllText("asciicid.txt");
+        Console.WriteLine(asciiImage);
+    }
+}
